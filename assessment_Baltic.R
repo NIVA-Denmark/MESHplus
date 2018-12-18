6481948627bf045b2e39c8645ff648898951c3e7
ciaran-dk <ciaran@murraynet.dk>
2018-12-18 17:50:21 +0100

    working added indicator grouping information

--- assessment_Baltic.R
+++ assessment_Baltic.R
@@ -1,10 +1,18 @@
+# 
 
 
+library(tidyverse)
 
+# 3 Quality Elements (QE)
+# Biology
+# Chemistry
+# Supporting
 
+# we apply one-out all-out between these 3 QEs
+# all other aggregation is done by averaging
 
 # Biodiversity (NEAT) already uses a 0.0-1.0 scale
-# conversion for HEAT+ and CHASE+ is done using the following fixed points:
+# conversion for HEAT+ Eutrophication Ratio (ER) and CHASE+ Contamination Ratio (CR) is done using the following fixed points:
 
   # EQR CHASE HEAT
   #     (CS)  (ER)
@@ -14,4 +22,20 @@
   # 0.6  1.0  1.0
   # 0.8  0.5  0.5
   # 1.0  0.0  0.0
+
+# Biology indicators
+dfBio<-read.table(file="data/JNCC/HELCOM_indicators.txt", 
+                        quote="",sep="\t",header=T,stringsAsFactors=F) %>%
+  select(GRIDCODE,Category,Group,Unit,Indicator,IndType,Bad,Threshold,Reference,Status)
+
+# Chemistry Indicators
+
+
+# Supporting Indicators
+
+
+# indicator group information
+dfGp<-read.table(file="data/indicator_groups_bio_supporting.txt", 
+               quote="",sep="\t", header=T, stringsAsFactors=F) 
+
 
--- data/HEAT/match_HELCOM_to_grid.R
+++ data/HEAT/match_HELCOM_to_grid.R
@@ -1,49 +1,29 @@
 library(tidyverse)
 
-EutClass<-function(ER){
-  Class<-ifelse(ER<=0.5,1,2)
-  Class<-ifelse(ER>1,3,Class)
-  Class<-ifelse(ER>1.5,4,Class)
-  Class<-ifelse(ER>2,5,Class)
-  Class<-ifelse(is.na(ER),NA,Class)
-  return(Class)
-}
 
-bWrite<-F #FALSE
-# --------------------------------------------------------
-coastweight <- 1 # weight of indicators from coastal areas relative to offshore when aggregating withig grid cell
-
-dfgridmatch<-read.table(file="data/grid_match.txt", 
+# table matching HELCOM shapes to EEA grid cells, with area of overlap
+dfgridmatch<-read.table(file="data/HEAT/grid_match.txt", 
                         quote="",sep=";", 
                         header=T, 
                         stringsAsFactors=F) %>% 
   select(Country=COUNTRY,ID,GRIDCODE,AREA=SHAPE_AREA)
 
-dfgrid<-read.table(file="data/grid_all.txt", 
-                   quote="",sep=";", 
-                   header=T, 
-                   stringsAsFactors=F) %>% 
-  select(GRIDCODE,Region=REGION)
-
-
-dfdat<-read.table(file="data/data_HELCOM_OSPAR.txt", 
+# data collected by JNCC 
+dfdat<-read.table(file="data/HEAT/data_HELCOM_OSPAR.txt", 
                   quote="",
                   sep="\t", 
                   header=T,
                   comment.char = "",
                   stringsAsFactors=F) %>% 
+  filter(Source=="HOLAS2EUTRO_20112016_Indicators.xlsx") %>%  # taking only HELCOM data
   filter(!is.na(Obs),!is.na(Threshold),Exclude==0) %>%
-  select(Country,ID,Category,OC,Parameter,Obs,Threshold,Response) %>%
-  mutate(weight=ifelse(OC=="C",coastweight,1)) %>%
-  select(-OC) #2585
+  select(Country,ID,Category,Parameter,Unit,Obs,Threshold,Response) 
 
-df <- dfgridmatch %>% left_join(dfdat,by=c("Country","ID")) %>% 
-  mutate(ER=ifelse(Response=="+",Obs/Threshold,Threshold/Obs)) %>%
-  filter(!is.na(ER))
+df <- dfgridmatch %>% left_join(dfdat,by=c("Country","ID")) 
 
-dfcat <- df %>% group_by(GRIDCODE,Category) %>%
-  mutate(AREA=1) %>% # effectively removes area weighting
-  summarise(sumER=sum(weight*ER*AREA,na.rm=T),sumAREA=sum(weight*AREA,na.rm=T)) %>%
-  mutate(ER=sumER/sumAREA) %>%
-  mutate(Class=EutClass(ER)) %>%
-  ungroup()
+df <- df %>% group_by(GRIDCODE,Category,Parameter,Unit,Response) %>%
+  summarise(Obs=mean(Obs,na.rm=T),Threshold=mean(Threshold,na.rm=T)) %>%
+  ungroup() %>%
+  filter(!is.na(Category))
+
+write.table(df,file="data/indicators_eutro.csv",sep="\t",fileEncoding="UTF-8",row.names=F,col.names=T,quote=F)
